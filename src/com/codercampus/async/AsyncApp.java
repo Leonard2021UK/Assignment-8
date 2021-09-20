9899b9a7c8b5997bcc75ebeecc70201899b10bb4
leo <otonemasubscriptions@gmail.com>
2021-09-20 19:07:16 +0100

    working versio

--- src/com/codercampus/async/AsyncApp.java
+++ src/com/codercampus/async/AsyncApp.java
@@ -1,17 +1,51 @@
 package com.codercampus.async;
 
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.Executor;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import javax.swing.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.stream.Stream;
 
 public class AsyncApp {
-    Assignment8 task = new Assignment8();
-    ExecutorService service = Executors.newCachedThreadPool();
 
-    CompletableFuture.supplyAsync(Assignment8::new);
+    public static void main(String[] args) {
+        // instantiate a task provider
+        Assignment8 taskProvider = new Assignment8();
+        // holds the number of ech type of number
+        // here I could use ConcurrentHashMap, thus I could eliminate the synchronised block in the loop
+        Map<Integer,Integer> intCounter = new HashMap<>();
+        // stores all CompletableFutures
+        List<CompletableFuture<Void>> futures = new ArrayList<>();
+        // creates a Cached thread pool
+        ExecutorService service = Executors.newCachedThreadPool();
+        // creates a CompletableFuture for each chunk of data
+        for (int i = 0;i < 1000;i++){
+            // processes a chunk of data (1000 numbers)
+            CompletableFuture<Void> future =  CompletableFuture.supplyAsync(()->taskProvider,service)
+                .thenApply(Assignment8::getNumbers)
+                .thenAccept((List<Integer> set) -> set.forEach(num ->{
+                    // synchronously counts each number's occurrence
+                    synchronized(intCounter){
+                        // increments the number of the corresponding number type by 1
+                        intCounter.merge(num,1,Integer::sum);
+                    }
+                }));
+            //  collects the created CompletableFuture
+            futures.add(future);
+        }
 
-    CompletableFuture<Assignment8> future
-        = CompletableFuture.supplyAsync(Assignment8::new);
+        // waits until all CompletableFuture are finished
+        for (CompletableFuture<Void> future : futures) {
+            future.join();
+        }
+
+        // prints the result
+        System.out.println(intCounter);
+        Integer sum=0;
+        for (Integer value:intCounter.values()){
+            sum = sum +value;
+        }
+
+        System.out.println(sum);
+    }
 
 }
